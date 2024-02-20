ddc8a1cbaabf42349b2f77a8c2e9c3b08a529d0d
Daniel Lopretto <dan@meterfeeder.com>
2024-02-20 14:44:37 -0500

    Hide main_content

--- theme/cyber_range/ajax/courses.php
+++ theme/cyber_range/ajax/courses.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/../../../config.php');
 require_login();
 global $DB, $USER;
 
-$sql  = "SELECT * FROM mdl_course WHERE 1";
+$sql  = "SELECT * FROM {course} WHERE 1";
 $records = $DB->get_records_sql($sql);
 header('content-type: application/json; charset=utf-8');
 echo json_encode($records);
