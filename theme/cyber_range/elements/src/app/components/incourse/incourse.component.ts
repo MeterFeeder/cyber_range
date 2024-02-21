a74ea6aad84585630bc3eb776e1acca9bff7fe65
DB <dylan@meterfeeder.com>
2024-02-21 15:19:19 -0500

    incourse page formatting and styling

--- theme/cyber_range/elements/src/app/components/incourse/incourse.component.ts
+++ theme/cyber_range/elements/src/app/components/incourse/incourse.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'ekc-incourse',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './incourse.component.html',
   styleUrl: './incourse.component.scss'
 })
