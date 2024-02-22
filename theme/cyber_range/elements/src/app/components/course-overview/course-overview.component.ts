28e59d0f3bde98fa47cf74aa0b486dc11a24f110
rconMF <regina@meterfeeder.com>
2024-02-22 15:07:32 -0500

    WIP trying to get carousel to function

--- theme/cyber_range/elements/src/app/components/course-overview/course-overview.component.ts
+++ theme/cyber_range/elements/src/app/components/course-overview/course-overview.component.ts
@@ -9,8 +9,44 @@ import { IconComponent } from '../icon/icon.component';
   styleUrl: './course-overview.component.scss'
 })
 export class CourseOverviewComponent {
+  public tempModules = [
+    {
+      "title": "1 - Test title",
+      "description": "Test description",
+      "number": 1
+    },
+    {
+      "title": "2 - Test title",
+      "description": "Test description",
+      "number": 2
+    },
+    {
+      "title": "3 - Test title",
+      "description": "Test description",
+      "number": 3
+    },
+    {
+      "title": "4 - Test title",
+      "description": "Test description",
+      "number": 4
+    },
+    {
+      "title": "5 - Test title",
+      "description": "Test description",
+      "number": 5
+    },
+  ]
 
-
+  public advance() {
+    console.log('i can get here?')
+    // let copy = [...this.tempModules];
+    // let first = copy.shift()
+    // if (first) {
+    //   copy.push(first);
+    //   console.log(copy);
+    //   this.tempModules = copy;
+    // }
+  }
 
 }
 
