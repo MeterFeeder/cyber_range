5a9d0a1022e5ad87ad72baac1da2390bb1b475a5
Daniel Lopretto <dan@meterfeeder.com>
2024-03-11 15:07:25 -0400

    Wrap the admin pages in cyber range theme

--- theme/cyber_range/config.php
+++ theme/cyber_range/config.php
@@ -118,7 +118,7 @@ $THEME->layouts = [
     // ),
     // Server administration scripts.
     'admin' => array(
-        'file' => 'columns2.php',
+        'file' => 'admin.php',
         'regions' => array('side-pre'),
         'defaultregion' => 'side-pre',
     ),
